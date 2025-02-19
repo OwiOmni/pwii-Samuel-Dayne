94fd88cf2705e5560ed6ed17120788fc3ec326a6
Samuel Dayne <samuel.dayne2008@gmail.com>
2025-02-19 10:34:02 -0300

    Aula de introdução ao PHP

---
+++ aula01/olamundo.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .text1
+        {
+            color: red;
+            font-size: 5rem;
+        }
+        .text2
+        {
+            color: green;
+            font-size: 6rem;
+        }
+        .text3
+        {
+            color: blue;
+            font-size: 6rem;
+        }
+        .text4
+        {
+            color: violet;
+            font-size: 6rem;
+        }
+    </style>
+    <body>
+        <p class="text1">
+            <?php
+                echo "My first PHP script!";
+            ?>
+        </p>
+        <p class="text2">
+            <?php
+                echo 3 + 3;
+            ?>
+        </p>
+        <p class="text3">
+            <?php
+                echo 10 / 3;
+            ?>
+        </p>
+    </body>
+</html>
