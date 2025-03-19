5d6af13775e07059c35beb2049dcc3aa8f6c0f67
Samuel Dayne <samuel.dayne2008@gmail.com>
2025-03-19 10:32:33 -0300

    Repetição

---
+++ aula04/repeticao.php
@@ -0,0 +1,22 @@
+<?php  
+$i = 1;
+
+while ($i < 20) {
+  echo $i;
+  $i++;
+} 
+
+echo "--------- <br>";
+
+$i = 1; 
+
+do{
+  echo $i;
+  $i++;
+}
+ while ($i < 20);
+ 
+for ($x = 0; $x <= 20; $x++) {
+  echo "The number is: $x <br>";
+}
+?>  
