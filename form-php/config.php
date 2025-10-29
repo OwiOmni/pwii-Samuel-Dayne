302244574817a200e081b047d2b7dcbefececf9a
Samuel Dayne <samuel.dayne2008@gmail.com>
2025-10-28 22:14:44 -0300

    Atividade banco de dados com PHP

---
+++ form-php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'UquetitaOMG';
+    // corrigido: nome do banco sem backtick extra
+    $dbName = 'formulario-samuel';
+
+    // Conexão com verificação de erro e charset
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    if ($conexao->connect_error) {
+        die('Conexão falhou: ' . $conexao->connect_error);
+    }
+    $conexao->set_charset('utf8mb4');
+
+?>
